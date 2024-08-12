d6a3b8a804d4336df0cb662ed27678beb86f37de
Athul H S <127603418+athulkadakkal@users.noreply.github.com>
2024-08-12 11:33:20 +0530

    Update LED-ON-OFF.ino

--- LED-ON-OFF.ino
+++ LED-ON-OFF.ino
@@ -1,25 +1,28 @@
-#define ledPin 7
+#include <SoftwareSerial.h>
+#define ledPin 13
 int state = 0;
+
+SoftwareSerial BTSerial(10, 11); // RX | TX
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-  Serial.begin(38400); // Default communication rate of the Bluetooth module
-}
+  Serial.begin(9600); // Default communication rate of the Bluetooth module
+  BTSerial.begin(9600);}
 
 void loop() {
-  if(Serial.available() > 0){ // Checks whether data is comming from the serial port
-    state = Serial.read(); // Reads the data from the serial port
+  if(BTSerial.available() > 0){ // Checks whether data is comming from the serial port
+    state = BTSerial.read(); // Reads the data from the serial port
  }
 
  if (state == '0') {
   digitalWrite(ledPin, LOW); // Turn LED OFF
   Serial.println("LED: OFF"); // Send back, to the phone, the String "LED: ON"
-  state = 0;
+  //state = 0;
  }
  else if (state == '1') {
   digitalWrite(ledPin, HIGH);
   Serial.println("LED: ON");;
-  state = 0;
+  //state = 0;
  } 
 }
